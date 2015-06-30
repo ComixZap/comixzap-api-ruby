9acad738ef4cb28db6a38c4f82234b6cca41f39b
Richard Hoffman <coverslide@gmail.com>
2015-06-29 23:05:04 -0700

    Add browser ui submodule

---
+++ scripts/install-ui.sh
@@ -0,0 +1,26 @@
+cwd=$(pwd)
+
+pushd $(dirname "$0") > /dev/null
+scriptdir=$(pwd)
+popd > /dev/null
+
+browserfolder="comixzap-browser-ui"
+
+rootdir=$(dirname "$scriptdir")
+browserdir="$rootdir/$browserfolder"
+
+cd "$rootdir"
+git submodule
+git submodule update --init
+
+cd "$browserdir"
+npm install
+echo '{}' > config.json
+./node_modules/.bin/gulp build
+
+
+if [ ] ; then
+  ln -s "$browserfolder/dist" "$rootdir/public" 
+fi
+
+cd "$cwd"
